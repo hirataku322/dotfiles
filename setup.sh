2d6a22133af3c0ad70b10510242e0fdd35c07b05
hyamakaw <hirotoyamakawa@yamakawatakutos-MacBook-Air.local>
2024-01-22 20:36:03 +0900

    add: starship.toml

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ ln -s "$(pwd)/init.vim" ~/.config/nvim
 ln -s "$(pwd)/tmux.conf" ~/.config/tmux
 ln -s "$(pwd)/alacritty.toml" ~/.config/alacritty
 ln -s "$(pwd)/.zshrc" ~/.zshrc
+ln -s "$(pwd)/starship.toml" ~/.config

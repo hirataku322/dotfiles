a90a5d3bbb80b07ef1275aecb7ca6fe4d8803e03
hyamakaw <hirotoyamakawa@yamakawatakutos-MacBook-Air.local>
2024-01-23 09:12:14 +0900

    delete: setup.sh

--- setup.sh
+++
@@ -1,5 +0,0 @@
-ln -s "$(pwd)/init.vim" ~/.config/nvim
-ln -s "$(pwd)/tmux.conf" ~/.config/tmux
-ln -s "$(pwd)/alacritty.toml" ~/.config/alacritty
-ln -s "$(pwd)/.zshrc" ~/.zshrc
-ln -s "$(pwd)/starship.toml" ~/.config

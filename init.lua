c44080f20feef2fcdea74bcd83b4c07f60d7b450
hyamakaw <hirataku322@gmail.com>
2024-02-03 19:41:33 +0900

    fix: init.luaを動く状態に

--- init.lua
+++ init.lua
@@ -112,15 +112,15 @@ require('telescope').setup{
 -- Toggleterm
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({
-	cmd = "lazygit",
-	direction = "float",
-	hidden = true
+  cmd = "lazygit",
+  direction = "float",
+  hidden = true
 })
 
 function _lazygit_toggle()
-	lazygit:toggle()
+  lazygit:toggle()
 end
-vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+map("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
 
 -- Nvim-web-Devicon
 require'nvim-web-devicons'.setup {

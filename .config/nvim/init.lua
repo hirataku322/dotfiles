60e9f0da7eb8480e9cd8311746d8696b1cfd4833
hyamakaw <hirataku322@gmail.com>
2024-02-06 00:05:07 +0900

    add: bufferline

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -224,3 +224,13 @@ require('lualine').setup {
 
 -- Oil
 require("oil").setup()
+
+-- Buffer Line
+require('bufferline').setup()
+vim.keymap.set('n', 'gb', '<CMD>BufferLinePick<CR>')
+vim.keymap.set('n', '<leader>ts', '<CMD>BufferLinePickClose<CR>')
+vim.keymap.set('n', '<S-l>', '<CMD>BufferLineCycleNext<CR>')
+vim.keymap.set('n', '<S-h>', '<CMD>BufferLineCyclePrev<CR>')
+vim.keymap.set('n', ']b', '<CMD>BufferLineMoveNext<CR>')
+vim.keymap.set('n', '[b', '<CMD>BufferLineMovePrev<CR>')
+vim.keymap.set('n', 'gs', '<CMD>BufferLineSortByDirectory<CR>')

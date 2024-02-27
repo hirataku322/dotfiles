c34c7fcbc0b8cd3e093650988ce7532b52b5d1ac
hyamakaw <hirataku322@gmail.com>
2024-02-27 13:34:20 +0900

    fix: bufferの移動に伴いproject-rootも変更

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -73,12 +73,22 @@ map('n', '<C-k>', ':bn<CR>', options)
 map('n', 'sd', ':bp|bd#<CR>', options)
 
 -- Telescope
-local telescope_options = {
-  cwd = string.gsub(vim.fn.system("git rev-parse --show-toplevel"), "\n", ""),
-  hidden = true
-}
-map('n', '<C-p>', function() require('telescope.builtin').find_files(telescope_options) end, options)
-map('n', '<leader>jg', function() require('telescope.builtin').live_grep(telescope_options) end, options)
+function get_git_root()
+  return string.gsub(vim.fn.system("git rev-parse --show-toplevel"), "\n", "")
+end
+
+function find_files()
+  local root = get_git_root()
+  require('telescope.builtin').find_files({cwd = root, hidden=true})
+end
+
+function live_grep()
+  local root = get_git_root()
+  require('telescope.builtin').live_grep({cwd = root, hidden=true})
+end
+
+map('n', '<C-p>', find_files, options)
+map('n', '<leader>jg', live_grep, options)
 map('n', '<leader>jb', require('telescope.builtin').buffers, options)
 map('n', '<leader>jh', ':Telescope help_tags<CR>', options)
 

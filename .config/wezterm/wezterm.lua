e7d7e2023bf82f3827c12d312c2133f90aded4dd
hyamakaw <hirataku322@gmail.com>
2024-06-08 15:35:00 +0900

    add: .wezterm.lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,4 +10,15 @@ local config = wezterm.config_builder()
 config.color_scheme = 'AdventureTime'
 
 -- and finally, return the configuration to wezterm
+
+-- 背景透過
+config.window_background_opacity = 0.9
+
+-- 最初からフルスクリーンで起動
+local mux = wezterm.mux
+wezterm.on("gui-startup", function(cmd)
+   local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
 return config

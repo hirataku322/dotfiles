0170e77d2de0b7099796080eebf164b8ccaf6b06
hyamakaw <hirataku322@gmail.com>
2024-08-09 22:35:18 +0900

    不要なPluginを削除

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -16,10 +16,8 @@ require("lazy").setup({
   'tpope/vim-surround',
   'easymotion/vim-easymotion',
   'numToStr/Comment.nvim',
-  'github/copilot.vim',
 
   -- Theme
-  'folke/tokyonight.nvim',
   'sheerun/vim-polyglot', -- old syntax highlighting
   { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
 
@@ -40,35 +38,5 @@ require("lazy").setup({
   {'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
 
   -- Other
-  'stevearc/overseer.nvim',
-  'mogelbrod/vim-jsonpath',
-  'jalvesaq/Nvim-R',
   {'akinsho/toggleterm.nvim', versoin = '*', config = true },
-
-  -- {
-  --   "jackMort/ChatGPT.nvim",
-  --     event = "VeryLazy",
-  --     config = function()
-  --       require("chatgpt").setup()
-  --     end,
-  --     dependencies = {
-  --       "MunifTanjim/nui.nvim",
-  --       "nvim-lua/plenary.nvim",
-  --       "folke/trouble.nvim",
-  --       "nvim-telescope/telescope.nvim"
-  --     }
-  -- },
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-    end,
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-  }
 })

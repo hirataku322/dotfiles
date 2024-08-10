b260d49a2dfe92f5d8df82af070e545a51705cdc
hyamakaw <hirataku322@gmail.com>
2024-08-11 02:38:36 +0900

    Add Complete and Snippets plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -27,7 +27,7 @@ require("lazy").setup({
   'nvim-tree/nvim-web-devicons',
   { 'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
 
-  -- Filer
+  -- File exproler
   { 'stevearc/oil.nvim', opts = {}, dependencies = "nvim-tree/nvim-web-devicons"},
 
   -- Telescope
@@ -39,4 +39,18 @@ require("lazy").setup({
   'williamboman/mason-lspconfig.nvim',
   'neovim/nvim-lspconfig',
   'simrat39/rust-tools.nvim',
+
+  -- Completion framework
+  'hrsh7th/nvim-cmp',
+
+  -- LSP completion source
+  'hrsh7th/cmp-nvim-lsp',
+
+  -- Useful completion sources
+  'hrsh7th/cmp-nvim-lua',
+  'hrsh7th/cmp-nvim-lsp-signature-help',
+  'hrsh7th/cmp-vsnip',                             
+  'hrsh7th/cmp-path',                              
+  'hrsh7th/cmp-buffer',                            
+  'hrsh7th/vim-vsnip',                             
 })

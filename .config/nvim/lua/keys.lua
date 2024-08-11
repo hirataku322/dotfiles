ae48c962676b8e4a9b90db84245026a44063ba94
hyamakaw <hirataku322@gmail.com>
2024-08-11 20:52:34 +0900

    Replace vim.keymap.set to map

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -1,36 +1,37 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 
+local map = vim.keymap.set
 options = {noremap = true}
 
 -- Mappings
-vim.keymap.set('n', 'k', 'gk', options)
-vim.keymap.set('n', 'j', 'gj', options)
-vim.keymap.set({'i', 'v'}, '<C-c>', '<ESC>', options)
-vim.keymap.set('n', '<C-l>', ':nohlsearch<CR>', options)
-vim.keymap.set('n', '<C-r>', ':w<CR>:luafile %<CR>:nohlsearch<CR>', options)
+map('n', 'k', 'gk', options)
+map('n', 'j', 'gj', options)
+map({'i', 'v'}, '<C-c>', '<ESC>', options)
+map('n', '<C-l>', ':nohlsearch<CR>', options)
+map('n', '<C-r>', ':w<CR>:luafile %<CR>:nohlsearch<CR>', options)
 
 -- Window
-vim.keymap.set('n', 'ss', ':sp<CR>', options)
-vim.keymap.set('n', 'sv', ':vs<CR>', options)
-vim.keymap.set('n', 'sq', ':q!<CR>', options)
-vim.keymap.set('n', 'sj', '<C-w>j', options)
-vim.keymap.set('n', 'sk', '<C-w>k', options)
-vim.keymap.set('n', 'sl', '<C-w>l', options)
-vim.keymap.set('n', 'sh', '<C-w>h', options)
-vim.keymap.set('n', 'sJ', '<C-w>J', options)
-vim.keymap.set('n', 'sK', '<C-w>K', options)
-vim.keymap.set('n', 'sL', '<C-w>L', options)
-vim.keymap.set('n', 'sH', '<C-w>H', options)
-vim.keymap.set('n', 's>', '<C-w>>', options)
-vim.keymap.set('n', 's<', '<C-w><', options)
-vim.keymap.set('n', 's+', '<C-w>+', options)
-vim.keymap.set('n', 's-', '<C-w>-', options)
+map('n', 'ss', ':sp<CR>', options)
+map('n', 'sv', ':vs<CR>', options)
+map('n', 'sq', ':q!<CR>', options)
+map('n', 'sj', '<C-w>j', options)
+map('n', 'sk', '<C-w>k', options)
+map('n', 'sl', '<C-w>l', options)
+map('n', 'sh', '<C-w>h', options)
+map('n', 'sJ', '<C-w>J', options)
+map('n', 'sK', '<C-w>K', options)
+map('n', 'sL', '<C-w>L', options)
+map('n', 'sH', '<C-w>H', options)
+map('n', 's>', '<C-w>>', options)
+map('n', 's<', '<C-w><', options)
+map('n', 's+', '<C-w>+', options)
+map('n', 's-', '<C-w>-', options)
 
 -- Buffer
-vim.keymap.set('n', '<C-j>', ':bp<CR>', options)
-vim.keymap.set('n', '<C-k>', ':bn<CR>', options)
-vim.keymap.set('n', 'sd', ':bp|bd#<CR>', options)
+map('n', '<C-j>', ':bp<CR>', options)
+map('n', '<C-k>', ':bn<CR>', options)
+map('n', 'sd', ':bp|bd#<CR>', options)
 
 -- Telescope
 function get_git_root()
@@ -44,16 +45,16 @@ end
 function live_grep()
   require('telescope.builtin').live_grep({cwd = get_git_root(), hidden=true})
 end
-vim.keymap.set('n', '<C-p>', find_files, options)
-vim.keymap.set('n', '<leader>jg', live_grep, options)
-vim.keymap.set('n', '<leader>jb', require('telescope.builtin').buffers, options)
-vim.keymap.set('n', '<leader>jh', require('telescope.builtin').help_tags, options)
+map('n', '<C-p>', find_files, options)
+map('n', '<leader>jg', live_grep, options)
+map('n', '<leader>jb', require('telescope.builtin').buffers, options)
+map('n', '<leader>jh', require('telescope.builtin').help_tags, options)
 
 -- Other
-vim.keymap.set("n", "<C-n>", "<CMD>Oil<CR>")
-vim.keymap.set('n', '<leader>f', '<Plug>(easymotion-bd-w)', options)
-vim.keymap.set("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", options)
+map("n", "<C-n>", "<CMD>Oil<CR>")
+map('n', '<leader>f', '<Plug>(easymotion-bd-w)', options)
+map("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", options)
 
 -- ToggleTerm
 require("toggleterm").setup{}
-vim.keymap.set("n", "<C-h>", ":ToggleTerm direction=float<CR>", options)
+map("n", "<C-h>", ":ToggleTerm direction=float<CR>", options)

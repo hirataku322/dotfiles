eeb2fe812aab2bfed55f5ee2878f350079077c4d
hyamakaw <hirataku322@gmail.com>
2024-08-11 20:58:17 +0900

    軽微な修正

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,8 +6,6 @@ function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
   vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
 end
-
--- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 -- Telecsope
@@ -84,6 +82,6 @@ require("oil").setup({
     }
   }
 )
-require'nvim-web-devicons'.setup {}
+require('nvim-web-devicons').setup() 
 require('bufferline').setup()
 require('Comment').setup()

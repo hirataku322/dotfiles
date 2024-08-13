f8d621b399ebf89b4ca387eb1dd0cf38af113733
hyamakaw <hirataku322@gmail.com>
2024-08-14 02:09:14 +0900

    cmpとtreesitterの設定値を分離

--- .config/nvim/lua/plugins/others.lua
+++ .config/nvim/lua/plugins/others.lua
@@ -11,25 +11,9 @@ return {
   'nvim-tree/nvim-web-devicons',
   { 'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
 
-  -- Treesitter
-  'nvim-treesitter/nvim-treesitter',
-
   -- LSP
   'neovim/nvim-lspconfig',
 
-  -- Completion framework
-  'hrsh7th/nvim-cmp',
-
-  -- LSP completion source
-  'hrsh7th/cmp-nvim-lsp',
-
-  -- Useful completion sources
-  'hrsh7th/cmp-nvim-lua',
-  'hrsh7th/cmp-nvim-lsp-signature-help',
-  'hrsh7th/cmp-vsnip',                             
-  'hrsh7th/cmp-path',                              
-  'hrsh7th/cmp-buffer',                            
-  'hrsh7th/vim-vsnip',                             
 
   -- Others
   'puremourning/vimspector',

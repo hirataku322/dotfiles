a9d71241fc1fc86e14fcfd3d86373e38c89d9ede
hyamakaw <hirataku322@gmail.com>
2024-08-14 01:40:12 +0900

    pluginのloadingをothers.luaに分離

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,284 +1,277 @@
-require('config.lazy')
 require('config.opts')
 require('config.keys')
+require('config.lazy')
 
--- Color Scheme
--- vim.cmd[[colorscheme nightfly]]
--- vim.g.nightflyTransparent = true
+-- -- Color Scheme
+vim.cmd[[colorscheme nightfly]]
+vim.g.nightflyTransparent = true
 
--- require("toggleterm").setup{}
--- map("n", "<C-h>", ":ToggleTerm direction=float<CR>", options)
--- function _G.set_terminal_keymaps()
---   local opts = {buffer = 0}
---   vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
--- end
--- vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+require("toggleterm").setup{}
+vim.keymap.set("n", "<C-h>", ":ToggleTerm direction=float<CR>", options)
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+end
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+-- Telecsope
+vim.g['findroot_not_for_subdir'] = 0
+vim.g['rooter_patterns'] = {'.git', '.svn', 'package.json', '!node_modules'}
+
+require("telescope").setup({
+  defaults = {
+    file_ignore_patterns = {
+      -- 検索から除外するものを指定
+      "^.git/",
+      "^.cache/",
+      "^Library/",
+      "Parallels",
+      "^Movies",
+      "^Music",
+    },
+    vimgrep_arguments = {
+      -- ripggrepコマンドのオプション
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "-uu",
+    },
+  },
+  extensions = {
+    -- ソート性能を大幅に向上させるfzfを使う
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = "smart_case",
+    },
+  },
+})
+require("telescope").load_extension("fzf")
+
+function get_git_root()
+  return string.gsub(vim.fn.system("git rev-parse --show-toplevel"), "\n", "")
+end
+
+function find_files()
+  require('telescope.builtin').find_files({cwd = get_git_root(), hidden=true})
+end
+
+function live_grep()
+  require('telescope.builtin').live_grep({cwd = get_git_root(), hidden=true})
+end
+vim.keymap.set('n', '<C-p>', find_files, options)
+vim.keymap.set('n', '<leader>jg', live_grep, options)
+vim.keymap.set('n', '<leader>jb', require('telescope.builtin').buffers, options)
+vim.keymap.set('n', '<leader>jh', require('telescope.builtin').help_tags, options)
+
+-- LazyGit
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({
+	cmd = "lazygit",
+	direction = "float",
+	hidden = true
+})
+
+function _lazygit_toggle()
+	lazygit:toggle()
+end
+vim.keymap.set("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", options)
+
+-- Lualine
+require('lualine').setup {
+  options = {
+    theme = 'nightfly',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''}
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  }
+}
+
+require('nvim-web-devicons').setup() 
+require('bufferline').setup()
+require('Comment').setup()
+
+-- Mason Setup
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "",
+            package_pending = "",
+            package_uninstalled = "",
+        },
+    }
+})
+require("mason-lspconfig").setup()
+
+-- Rust Tools
+local rt = require("rust-tools")
+
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
+
+-- LSP Diagnostics Options Setup 
+local sign = function(opts)
+  vim.fn.sign_define(opts.name, {
+    texthl = opts.name,
+    text = opts.text,
+    numhl = ''
+  })
+end
+
+sign({name = 'DiagnosticSignError', text = ''})
+sign({name = 'DiagnosticSignWarn', text = ''})
+sign({name = 'DiagnosticSignHint', text = ''})
+sign({name = 'DiagnosticSignInfo', text = ''})
+
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    update_in_insert = true,
+    underline = true,
+    severity_sort = false,
+    float = {
+        border = 'rounded',
+        source = 'always',
+        header = '',
+        prefix = '',
+    },
+})
+
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+--Set completeopt to have a better completion experience
 --
--- -- Telecsope
--- vim.g['findroot_not_for_subdir'] = 0
--- vim.g['rooter_patterns'] = {'.git', '.svn', 'package.json', '!node_modules'}
---
--- require("telescope").setup({
---   defaults = {
---     file_ignore_patterns = {
---       -- 検索から除外するものを指定
---       "^.git/",
---       "^.cache/",
---       "^Library/",
---       "Parallels",
---       "^Movies",
---       "^Music",
---     },
---     vimgrep_arguments = {
---       -- ripggrepコマンドのオプション
---       "rg",
---       "--color=never",
---       "--no-heading",
---       "--with-filename",
---       "--line-number",
---       "--column",
---       "--smart-case",
---       "-uu",
---     },
---   },
---   extensions = {
---     -- ソート性能を大幅に向上させるfzfを使う
---     fzf = {
---       fuzzy = true,
---       override_generic_sorter = true,
---       override_file_sorter = true,
---       case_mode = "smart_case",
---     },
---   },
--- })
--- require("telescope").load_extension("fzf")
---
--- function get_git_root()
---   return string.gsub(vim.fn.system("git rev-parse --show-toplevel"), "\n", "")
--- end
---
--- function find_files()
---   require('telescope.builtin').find_files({cwd = get_git_root(), hidden=true})
--- end
---
--- function live_grep()
---   require('telescope.builtin').live_grep({cwd = get_git_root(), hidden=true})
--- end
--- map('n', '<C-p>', find_files, options)
--- map('n', '<leader>jg', live_grep, options)
--- map('n', '<leader>jb', require('telescope.builtin').buffers, options)
--- map('n', '<leader>jh', require('telescope.builtin').help_tags, options)
---
--- -- LazyGit
--- local Terminal = require("toggleterm.terminal").Terminal
--- local lazygit = Terminal:new({
--- 	cmd = "lazygit",
--- 	direction = "float",
--- 	hidden = true
--- })
---
--- function _lazygit_toggle()
--- 	lazygit:toggle()
--- end
--- map("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", options)
---
--- -- Lualine
--- require('lualine').setup {
---   options = {
---     theme = 'nightfly',
---     component_separators = { left = '', right = ''},
---     section_separators = { left = '', right = ''}
---   },
---   sections = {
---     lualine_a = {'mode'},
---     lualine_b = {'branch', 'diff', 'diagnostics'},
---     lualine_c = {'filename'},
---     lualine_x = {'filetype'},
---     lualine_y = {'progress'},
---     lualine_z = {'location'}
---   }
--- }
---
--- require("oil").setup({
---     view_options = {
---       show_hidden = true
---     }
---   }
--- )
--- map("n", "<C-n>", "<CMD>Oil<CR>")
--- require('nvim-web-devicons').setup() 
--- require('bufferline').setup()
--- require('Comment').setup()
---
--- -- Mason Setup
--- require("mason").setup({
---     ui = {
---         icons = {
---             package_installed = "",
---             package_pending = "",
---             package_uninstalled = "",
---         },
---     }
--- })
--- require("mason-lspconfig").setup()
---
--- -- Rust Tools
--- local rt = require("rust-tools")
---
--- rt.setup({
---   server = {
---     on_attach = function(_, bufnr)
---       -- Hover actions
---       vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
---       -- Code action groups
---       vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
---     end,
---   },
--- })
---
--- -- LSP Diagnostics Options Setup 
--- local sign = function(opts)
---   vim.fn.sign_define(opts.name, {
---     texthl = opts.name,
---     text = opts.text,
---     numhl = ''
---   })
--- end
---
--- sign({name = 'DiagnosticSignError', text = ''})
--- sign({name = 'DiagnosticSignWarn', text = ''})
--- sign({name = 'DiagnosticSignHint', text = ''})
--- sign({name = 'DiagnosticSignInfo', text = ''})
---
--- vim.diagnostic.config({
---     virtual_text = false,
---     signs = true,
---     update_in_insert = true,
---     underline = true,
---     severity_sort = false,
---     float = {
---         border = 'rounded',
---         source = 'always',
---         header = '',
---         prefix = '',
---     },
--- })
---
--- vim.cmd([[
--- set signcolumn=yes
--- autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
--- ]])
---
--- --Set completeopt to have a better completion experience
--- --
--- -- menuone: popup even when there's only one match
--- -- noinsert: Do not insert text until a selection is made
--- -- noselect: Do not select, force to select one from the menu
--- -- shortness: avoid showing extra messages when using completion
--- -- updatetime: set updatetime for CursorHold
--- vim.opt.completeopt = {'menuone', 'noselect', 'noinsert'}
--- vim.opt.shortmess = vim.opt.shortmess + { c = true}
--- vim.api.nvim_set_option('updatetime', 300) 
---
--- -- Fixed column for diagnostics to appear
--- -- Show autodiagnostic popup on cursor hover_range
--- -- Goto previous / next diagnostic warning / error 
--- -- Show inlay_hints more frequently 
--- vim.cmd([[
--- set signcolumn=yes
--- autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
--- ]])
---
--- -- Completion Plugin Setup
--- local cmp = require'cmp'
--- cmp.setup({
---   -- Enable LSP snippets
---   snippet = {
---     expand = function(args)
---         vim.fn["vsnip#anonymous"](args.body)
---     end,
---   },
---   mapping = {
---     ['<C-p>'] = cmp.mapping.select_prev_item(),
---     ['<C-n>'] = cmp.mapping.select_next_item(),
---     -- Add tab support
---     ['<S-Tab>'] = cmp.mapping.select_prev_item(),
---     ['<Tab>'] = cmp.mapping.select_next_item(),
---     ['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
---     ['<C-f>'] = cmp.mapping.scroll_docs(4),
---     ['<C-Space>'] = cmp.mapping.complete(),
---     ['<C-e>'] = cmp.mapping.close(),
---     ['<CR>'] = cmp.mapping.confirm({
---       behavior = cmp.ConfirmBehavior.Insert,
---       select = true,
---     })
---   },
---   -- Installed sources:
---   sources = {
---     { name = 'path' },                              -- file paths
---     { name = 'nvim_lsp', keyword_length = 3 },      -- from language server
---     { name = 'nvim_lsp_signature_help'},            -- display function signatures with current parameter emphasized
---     { name = 'nvim_lua', keyword_length = 2},       -- complete neovim's Lua runtime API such vim.lsp.*
---     { name = 'buffer', keyword_length = 2 },        -- source current buffer
---     { name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip 
---     { name = 'calc'},                               -- source for math calculation
---   },
---   window = {
---       completion = cmp.config.window.bordered(),
---       documentation = cmp.config.window.bordered(),
---   },
---   formatting = {
---       fields = {'menu', 'abbr', 'kind'},
---       format = function(entry, item)
---           local menu_icon ={
---               nvim_lsp = 'λ',
---               vsnip = '⋗',
---               buffer = 'Ω',
---               path = '🖫',
---           }
---           item.menu = menu_icon[entry.source.name]
---           return item
---       end,
---   },
--- })
---
--- -- Treesitter Plugin Setup 
--- require('nvim-treesitter.configs').setup {
---   ensure_installed = { "lua", "rust", "toml" },
---   auto_install = true,
---   highlight = {
---     enable = true,
---     additional_vim_regex_highlighting=false,
---   },
---   ident = { enable = true }, 
---   rainbow = {
---     enable = true,
---     extended_mode = true,
---     max_file_lines = nil,
---   }
--- }
---
--- -- Vimsperctor options
--- vim.cmd([[
--- let g:vimspector_sidebar_width = 85
--- let g:vimspector_bottombar_height = 15
--- let g:vimspector_terminal_maxwidth = 70
--- ]])
---
--- map('n', "Db", ":call vimspector#ToggleBreakpoint()<cr>")
--- map('n', "Dw", ":call vimspector#AddWatch()<cr>")
--- map('n', "De", ":call vimspector#Evaluate()<cr>")
---
--- -- Vimspector
--- vim.cmd([[
--- nmap <F9> <cmd>call vimspector#Launch()<cr>
--- nmap <F5> <cmd>call vimspector#StepOver()<cr>
--- nmap <F8> <cmd>call vimspector#Reset()<cr>
--- nmap <F11> <cmd>call vimspector#StepOver()<cr>")
--- nmap <F12> <cmd>call vimspector#StepOut()<cr>")
--- nmap <F10> <cmd>call vimspector#StepInto()<cr>")
--- ]])
---
+-- menuone: popup even when there's only one match
+-- noinsert: Do not insert text until a selection is made
+-- noselect: Do not select, force to select one from the menu
+-- shortness: avoid showing extra messages when using completion
+-- updatetime: set updatetime for CursorHold
+vim.opt.completeopt = {'menuone', 'noselect', 'noinsert'}
+vim.opt.shortmess = vim.opt.shortmess + { c = true}
+vim.api.nvim_set_option('updatetime', 300) 
+
+-- Fixed column for diagnostics to appear
+-- Show autodiagnostic popup on cursor hover_range
+-- Goto previous / next diagnostic warning / error 
+-- Show inlay_hints more frequently 
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+-- Completion Plugin Setup
+local cmp = require'cmp'
+cmp.setup({
+  -- Enable LSP snippets
+  snippet = {
+    expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    -- Add tab support
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    })
+  },
+  -- Installed sources:
+  sources = {
+    { name = 'path' },                              -- file paths
+    { name = 'nvim_lsp', keyword_length = 3 },      -- from language server
+    { name = 'nvim_lsp_signature_help'},            -- display function signatures with current parameter emphasized
+    { name = 'nvim_lua', keyword_length = 2},       -- complete neovim's Lua runtime API such vim.lsp.*
+    { name = 'buffer', keyword_length = 2 },        -- source current buffer
+    { name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip 
+    { name = 'calc'},                               -- source for math calculation
+  },
+  window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+  },
+  formatting = {
+      fields = {'menu', 'abbr', 'kind'},
+      format = function(entry, item)
+          local menu_icon ={
+              nvim_lsp = 'λ',
+              vsnip = '⋗',
+              buffer = 'Ω',
+              path = '🖫',
+          }
+          item.menu = menu_icon[entry.source.name]
+          return item
+      end,
+  },
+})
+
+-- Treesitter Plugin Setup 
+require('nvim-treesitter.configs').setup {
+  ensure_installed = { "lua", "rust", "toml" },
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting=false,
+  },
+  ident = { enable = true }, 
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil,
+  }
+}
+
+-- Vimsperctor options
+vim.cmd([[
+let g:vimspector_sidebar_width = 85
+let g:vimspector_bottombar_height = 15
+let g:vimspector_terminal_maxwidth = 70
+]])
+
+vim.keymap.set('n', "Db", ":call vimspector#ToggleBreakpoint()<cr>")
+vim.keymap.set('n', "Dw", ":call vimspector#AddWatch()<cr>")
+vim.keymap.set('n', "De", ":call vimspector#Evaluate()<cr>")
+
+-- Vimspector
+vim.cmd([[
+nmap <F9> <cmd>call vimspector#Launch()<cr>
+nmap <F5> <cmd>call vimspector#StepOver()<cr>
+nmap <F8> <cmd>call vimspector#Reset()<cr>
+nmap <F11> <cmd>call vimspector#StepOver()<cr>")
+nmap <F12> <cmd>call vimspector#StepOut()<cr>")
+nmap <F10> <cmd>call vimspector#StepInto()<cr>")
+]])
+
 
 -- easy-motion
--- map('n', '<leader>f', '<Plug>(easymotion-bd-w)', options)
+vim.keymap.set('n', '<leader>f', '<Plug>(easymotion-bd-w)', options)

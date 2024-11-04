e795f95f20ecf750a4d39af90228ffddfdd82941
hyamakaw <hirataku322@gmail.com>
2024-11-04 18:51:08 +0900

    ターミナルエミュレーターをweztermに移行する

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,73 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+-- font
+config.font_size = 13
+config.font = wezterm.font 'JetBrains Mono'
+config.use_ime = true
+
+-- colors & appearance
+config.color_scheme = 'AdventureTime'
+config.window_background_opacity = 0.85
+config.macos_window_background_blur = 20
+
+-- tab bar
+config.window_decorations = "RESIZE"
+config.show_tabs_in_tab_bar = false
+
+-- keymap
+config.leader = {key = 'q', mods='CTRL'}
+config.keys = {
+  {
+    key = '¥',
+    mods = 'LEADER',
+    action = wezterm.action.SplitHorizontal {domain = 'CurrentPaneDomain'},
+  },
+  {
+    key = '-',
+    mods = 'LEADER',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'x',
+    mods = 'LEADER',
+    action = wezterm.action.CloseCurrentPane {confirm = false} ,
+  },
+  {
+    key = 'h',
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'l',
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection 'Right',
+  },
+  {
+    key = 'k',
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'j',
+    mods = 'LEADER',
+    action = wezterm.action.ActivatePaneDirection 'Down',
+  },
+  {
+    key = 'c',
+    mods = 'LEADER',
+    action = wezterm.action.SpawnCommandInNewTab,
+  },
+  { 
+    key = 'n', 
+    mods = 'LEADER', 
+    action = wezterm.action.ActivateTabRelative(1) 
+  },
+  { 
+    key = '[', 
+    mods = 'LEADER', 
+    action = wezterm.action.ActivateCopyMode 
+  },
+}
+
+return config

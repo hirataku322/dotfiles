3753834d942cc143f8515ae8c8b8570abbf3543f
hyamakaw <hirataku322@gmail.com>
2025-01-25 14:44:29 +0900

    change show parent directory and filename

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -11,7 +11,13 @@ return {
         sections = {
           lualine_a = {'mode'},
           lualine_b = {'branch', 'diff', 'diagnostics'},
-          lualine_c = {'filename'},
+          -- lualine_c = {'filename'},
+          lualine_c = {
+            {
+              'filename',
+              path = 4,
+            }
+          },
           lualine_x = {'filetype'},
           lualine_y = {'progress'},
           lualine_z = {'location'}

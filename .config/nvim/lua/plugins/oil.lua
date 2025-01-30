2e711e6077e1e6c3e3ad6fce675f8da0faa7712c
hyamakaw <hirataku322@gmail.com>
2025-01-31 00:34:10 +0900

    ファイルの変更を検知してoilをリロードする

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -5,6 +5,7 @@ return {
       delete_to_trash = true,
       skip_confirm_for_simple_edits = true,
       prompt_save_on_select_new_entry = false,
+      watch_for_changes= true,
       view_options = {
         show_hidden = true
       }

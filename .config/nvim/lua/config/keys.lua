e05988973a1f418c049d8b97edd0a94de1ae6089
hyamakaw <hirataku322@gmail.com>
2025-02-02 13:13:12 +0900

    lspのkeymapsの重複を排除

--- .config/nvim/lua/config/keys.lua
+++ .config/nvim/lua/config/keys.lua
@@ -3,7 +3,7 @@ vim.g.maplocalleader = ','
 
 vim.keymap.set('n', 'k', 'gk')
 vim.keymap.set('n', 'j', 'gj')
-vim.keymap.set({'i', 'v'}, '<C-c>', '<ESC>')
+vim.keymap.set({ 'i', 'v' }, '<C-c>', '<ESC>')
 vim.keymap.set('n', '<C-l>', ':nohlsearch<CR>')
 
 vim.keymap.set('n', 'ss', ':sp<CR>')
@@ -21,14 +21,3 @@ vim.keymap.set('n', 's-', '<C-w>-')
 vim.keymap.set('n', '<C-j>', ':bp<CR>')
 vim.keymap.set('n', '<C-k>', ':bn<CR>')
 vim.keymap.set('n', 'sd', ':bp|bd#<CR>')
-
-vim.keymap.set('n', 'gd', '<Plug>(lsp-definition)')
-vim.keymap.set('n', 'gs', '<Plug>(lsp-document-symbol-search)')
-vim.keymap.set('n', 'gS', '<Plug>(lsp-workspace-symbol-search)')
-vim.keymap.set('n', 'gr', '<Plug>(lsp-references)')
-vim.keymap.set('n', 'gi', '<Plug>(lsp-implementation)')
-vim.keymap.set('n', 'gt', '<Plug>(lsp-type-definition)')
-vim.keymap.set('n', 'gn', '<Plug>(lsp-rename)')
-vim.keymap.set('n', '[g', '<Plug>(lsp-previous-diagnostic)')
-vim.keymap.set('n', ']g', '<Plug>(lsp-next-diagnostic)')
-vim.keymap.set('n', 'K',  '<Plug>(lsp-hover)')

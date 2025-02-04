0da9b12c995657d6f453fe14f2f8efec299f6f4d
hyamakaw <hirataku322@gmail.com>
2025-02-04 22:47:06 +0900

    Resolve conflict between conform.nvim and copilot.vim

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -29,8 +29,6 @@ return {
 					["<C-p>"] = cmp.mapping.select_prev_item(),
 					["<C-n>"] = cmp.mapping.select_next_item(),
 					-- Add tab support
-					["<S-Tab>"] = cmp.mapping.select_prev_item(),
-					["<Tab>"] = cmp.mapping.select_next_item(),
 					["<C-S-f>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),

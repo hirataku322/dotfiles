834d8c70d48ee589762c3d73e17ae3d646ca0d74
hirataku322 <hirataku322@gmail.com>
2025-11-02 22:09:08 +0900

    add git worktree関連のalias

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 SOURCE_DIR=$(git rev-parse --show-toplevel)
 ln -Fs ${SOURCE_DIR}/.wezterm.lua ~/.wezterm.lua
 ln -Fs ${SOURCE_DIR}/.zshrc ~/.zshrc
+ln -Fs ${SOURCE_DIR}/.gitconfig ~/.gitconfig
 ln -Fs ${SOURCE_DIR}/.config/starship.toml ~/.config/starship.toml
 ln -Fs ${SOURCE_DIR}/.config/nvim ~/.config/nvim
 ln -Fs ${SOURCE_DIR}/.config/github-copilot ~/.config/github-copilot

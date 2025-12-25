e7d810af4640d06f104b72097561227b272ec21d
hirataku322 <hirataku322@gmail.com>
2025-12-25 11:45:06 +0900

    refactor フォーマット

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,73 +1,74 @@
 return {
-	{
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "make",
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		opts = {
-			defaults = {
-				file_ignore_patterns = {
-					-- 検索から除外するものを指定
-					"^.git/",
-					"^node_modules/",
-					"^.next/",
-					"^.venv/",
-					"^.DS_Store",
-					"^.cache/",
-					"^Library/",
-					"Parallels",
-					"^Movies",
-					"^Music",
-				},
-				vimgrep_arguments = {
-					-- ripggrepコマンドのオプション
-					"rg",
-					"--color=never",
-					"--no-heading",
-					"--with-filename",
-					"--line-number",
-					"--column",
-					"--smart-case",
-					"-uu",
-				},
-			},
-			extensions = {
-				-- ソート性能を大幅に向上させるfzfを使う
-				fzf = {
-					fuzzy = true,
-					override_generic_sorter = true,
-					override_file_sorter = true,
-					case_mode = "smart_case",
-				},
-			},
-		},
-		config = function(_, opts)
-			require("telescope").setup(opts)
-			require("telescope").load_extension("fzf")
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    opts = {
+      defaults = {
+        file_ignore_patterns = {
+          -- 検索から除外するものを指定
+          "^.git/",
+          "^node_modules/",
+          "^.next/",
+          "^.venv/",
+          "^.DS_Store",
+          "^.cache/",
+          "^Library/",
+          "Parallels",
+          "^Movies",
+          "^Music",
+        },
+        vimgrep_arguments = {
+          -- ripggrepコマンドのオプション
+          "rg",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          "-uu",
+        },
+      },
+      extensions = {
+        -- ソート性能を大幅に向上させるfzfを使う
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        },
+      },
+    },
+    config = function(_, opts)
+      require("telescope").setup(opts)
+      require("telescope").load_extension("fzf")
 
-			vim.g["findroot_not_for_subdir"] = 0
-			vim.g["rooter_patterns"] = { ".git", ".svn", "package.json", "!node_modules" }
+      vim.g["findroot_not_for_subdir"] = 0
+      vim.g["rooter_patterns"] = { ".git", ".svn", "package.json", "!node_modules" }
 
-			function get_git_root()
-				return string.gsub(vim.fn.system("git rev-parse --show-toplevel"), "\n", "")
-			end
+      function get_git_root()
+        return string.gsub(vim.fn.system("git rev-parse --show-toplevel"), "\n", "")
+      end
 
-			function find_files()
-				require("telescope.builtin").find_files({ cwd = get_git_root(), hidden = true })
-			end
+      function find_files()
+        require("telescope.builtin").find_files({ cwd = get_git_root(), hidden = true })
+      end
 
-			function live_grep()
-				require("telescope.builtin").live_grep({ cwd = get_git_root(), hidden = true })
-			end
-			vim.keymap.set("n", "<C-p>", find_files, options)
-			vim.keymap.set("n", "<leader>jg", live_grep, options)
-			vim.keymap.set("n", "<leader>jb", require("telescope.builtin").buffers, options)
-			vim.keymap.set("n", "<leader>jh", require("telescope.builtin").help_tags, options)
-		end,
-	},
-	{
-		"nvim-lua/plenary.nvim",
-	},
+      function live_grep()
+        require("telescope.builtin").live_grep({ cwd = get_git_root(), hidden = true })
+      end
+
+      vim.keymap.set("n", "<C-p>", find_files, options)
+      vim.keymap.set("n", "<leader>jg", live_grep, options)
+      vim.keymap.set("n", "<leader>jb", require("telescope.builtin").buffers, options)
+      vim.keymap.set("n", "<leader>jh", require("telescope.builtin").help_tags, options)
+    end,
+  },
+  {
+    "nvim-lua/plenary.nvim",
+  },
 }

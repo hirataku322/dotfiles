13a7b8e11dbefad5e94441e9f92557e1a3e9b4aa
hirataku322 <hirataku322@gmail.com>
2026-03-15 00:10:20 +0900

    Revert "refactor: codecompanion.nvimプラグイン設定を削除"
    This reverts commit 01d6a60fe3806723c56c69e06d5f95e9e142f4d9.

---
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,56 @@
+return {
+  "olimorris/codecompanion.nvim",
+  version = "^19.0.0",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "franco-ruggeri/codecompanion-spinner.nvim",
+  },
+  config = function()
+    require("codecompanion").setup({
+      interactions = {
+        chat = {
+          adapter = {
+            name = "opencode",
+          },
+          opts = {
+            completion_provider = "cmp", -- fixed typo
+          },
+          keymaps = {
+            send = {
+              modes = { n = "<C-s>", i = "<C-s>" },
+            },
+            close = {
+              modes = { n = "<C-c>", i = "<C-a>" },
+            },
+          },
+        },
+      },
+      display = {
+        chat = {
+          window = {
+            layout = "float",
+            width = 0.95,
+            height = 0.95,
+          },
+          show_context = false,
+        },
+      },
+      opts = {
+        language = "Japanese",
+        send_code = false
+      },
+      extensions = {
+        spinner = {}
+      }
+    })
+
+    -- Keymaps for chat toggle and add
+    local keymap_opts = { noremap = true, silent = true }
+    vim.keymap.set({ "n", "v" }, "<C-,>", "<cmd>CodeCompanionChat Toggle<cr>", keymap_opts)
+    vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", keymap_opts)
+
+    -- Expand 'cc' into 'CodeCompanion' in the command line
+    vim.cmd([[cab cc CodeCompanion]])
+  end,
+}

11ee25d398ebd550a812dc7807811fede2d1a47d
hirataku322 <hirataku322@gmail.com>
2026-03-14 19:44:31 +0900

    feat: CodeCompanionのウィンドウレイアウトとキーマップを設定

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -12,21 +12,40 @@ return {
           adapter = {
             name = "opencode",
           },
+          opts = {
+            completion_provider = "cmp", -- fixed typo
+          },
           keymaps = {
             send = {
               modes = { n = "<C-s>", i = "<C-s>" },
             },
             close = {
-              modes = { n = "<C-c>", i = "<C-a>" }
+              modes = { n = "<C-c>", i = "<C-a>" },
             },
           },
-        }
+        },
+      },
+      display = {
+        chat = {
+          window = {
+            layout = "float",
+            width = 0.95,
+            height = 0.95,
+          },
+          show_context = false,
+        },
+      },
+      opts = {
+        language = "Japanese"
       }
     })
-    vim.keymap.set({ "n", "v" }, "<C-,>", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
-    vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+    -- Keymaps for chat toggle and add
+    local keymap_opts = { noremap = true, silent = true }
+    vim.keymap.set({ "n", "v" }, "<C-,>", "<cmd>CodeCompanionChat Toggle<cr>", keymap_opts)
+    vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", keymap_opts)
 
     -- Expand 'cc' into 'CodeCompanion' in the command line
     vim.cmd([[cab cc CodeCompanion]])
-  end
+  end,
 }

cb4b2d8e23eaf8a2efac2578f8459e6695f37151
hirataku322 <hirataku322@gmail.com>
2026-03-14 19:01:55 +0900

    feat: codecompanion.nvimを追加し、プラグインを更新

---
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,32 @@
+return {
+  "olimorris/codecompanion.nvim",
+  version = "^19.0.0",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("codecompanion").setup({
+      interactions = {
+        chat = {
+          adapter = {
+            name = "opencode",
+          },
+          keymaps = {
+            send = {
+              modes = { n = "<C-s>", i = "<C-s>" },
+            },
+            close = {
+              modes = { n = "<C-c>", i = "<C-a>" }
+            },
+          },
+        }
+      }
+    })
+    vim.keymap.set({ "n", "v" }, "<C-,>", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+    vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+    -- Expand 'cc' into 'CodeCompanion' in the command line
+    vim.cmd([[cab cc CodeCompanion]])
+  end
+}
